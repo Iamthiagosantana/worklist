6a8ede063af223f0b0c10b008312d002b551d3b2
thiago <thiago.desantana@jala.university>
2026-09-12 19:00:15 -0300

    chore: implemented backend connection through frontend

--- backend/WorkList.Api/Program.cs
+++ backend/WorkList.Api/Program.cs
@@ -7,6 +7,19 @@ builder.Services.AddDbContext<WorkListDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Frontend", policy =>
+    {
+        policy
+            .WithOrigins(
+                "http://localhost:5173"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -18,6 +31,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("Frontend");
 
 app.UseHttpsRedirection();
 
