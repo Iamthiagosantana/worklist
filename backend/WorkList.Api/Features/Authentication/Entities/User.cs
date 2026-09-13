d6c012175fdc1bea5c04e4e40e56e80248dc141c
thiago <thiago.desantana@jala.university>
2026-09-12 23:32:34 -0300

    feat: created user entity

---
+++ backend/WorkList.Api/Features/Authentication/Entities/User.cs
@@ -0,0 +1,10 @@
+namespace WorkList.Api.Features.Authentication.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+}
